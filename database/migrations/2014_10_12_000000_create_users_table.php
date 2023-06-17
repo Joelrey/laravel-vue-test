026572a8b39f804ad5238e76540f4938990c8d7a
unknown <joelreycamacho@gmail.com>
2023-06-17 19:51:28 +0800

    reduced email length in migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username', 50)->unique();
-            $table->string('email')->unique();
-            $table->string('phone')->nullable();
+            $table->string('email', 100)->unique();
+            $table->string('phone', 20)->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 191);
             $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
